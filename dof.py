ba5f3d6a3a0104689059ba990bc4482de9f2993e
Stavros Varvatsoulis <svarvatsoulis@gmail.com>
2026-01-05 12:12:24 -0500

    drew outline

--- dof.py
+++ dof.py
@@ -19,3 +19,20 @@ def draw_flow(img, flow, step=16):
         cv2.circle(img_br, (x1, y1), 1, (0, 255, 0), -1)
         
     return img_br
+
+
+def draw_hsv(flow):
+    h, w = flow.shape[:2]
+    fx = flow[:,:,0]
+    fy = flow[:,:,1]
+    
+    angle = np.arctan2(fy, fx) + np.pi
+    v = np.sqrt(fx*fx*+fy*fy)
+    
+    hsv = np.zeros((h, w, 3), np.uint8)
+    hsv[...,0] = angle*(180/np.pi/2)
+    hsv[...,1] = 255
+    hsv[...,2] = np.minimum(v*4, 255)
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    
+    return bgr
