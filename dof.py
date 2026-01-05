bfe4dd645286c9cf775dc5ebbeb38375680925c5
Stavros Varvatsoulis <svarvatsoulis@gmail.com>
2026-01-05 11:57:37 -0500

    implemented gray background for when we start

--- dof.py
+++ dof.py
@@ -4,4 +4,11 @@ import time
 
 
 def draw_flow(img, flow, step=16):
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1).astype(int)
+    fx, fy = flow[y,x].T
     
+    lines = np.vstack([x, y, x-fx, y-fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    
+    img_br = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
